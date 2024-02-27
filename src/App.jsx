a8f7d929a46a1b982eb41cc7b3b2c276b1062a3a
tjordan4 <tysoncjordan@gmail.com>
2024-02-27 11:29:14 -0700

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import AllPlayers from './components/AllPlayers'
 import SinglePlayer from './components/SinglePlayer'
+import NavBar from './components/NavBar'
 
 const APIURL = "https://fsa-puppy-bowl.herokuapp.com/api/tjordan4/players" 
 function App() {
 
   return (
     <>
+      <header>
+        <h1>Welcome to the 2024 Puppy Bowl!</h1>
+        <h3>Sponsored by the Humane Society of Utah</h3>
+      </header>
+      <NavBar className='navbar' />
       <Routes>
         <Route path='/' element={<AllPlayers/>} />
         <Route path='/players/:id element={<SinglePlayer />}'/>
