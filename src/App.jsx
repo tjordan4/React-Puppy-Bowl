124235700330878dd5d171ebd5dbe565b182c24d
tjordan4 <tysoncjordan@gmail.com>
2024-02-27 08:48:50 -0700

    declared APIURL

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-
 import './App.css'
 
+const APIURL = "https://fsa-puppy-bowl.herokuapp.com/api/tjordan4/players" 
 function App() {
   const [count, setCount] = useState(0)
 
